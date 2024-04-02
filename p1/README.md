78969d36d74c6687559ee2c109daaeaa6e9d7ac7
Jorge Eduardo Ardila <jea.data@gmail.com>
2024-04-01 19:42:49 -0500

    added tts, openai, environ, apikey

--- p1/README.md
+++ p1/README.md
@@ -7,3 +7,6 @@ finally assembles the video with overlay - soft or hard subtitles.
 - [Voice
   Cloning](https://marcocaggiano.medium.com/step-by-step-tutorial-on-how-to-clone-your-voice-using-python-and-ai-f95a8896d56c)
   [Tutorial](https://www.digitalocean.com/community/tutorials/how-to-generate-and-add-subtitles-to-videos-using-python-openai-whisper-and-ffmpeg)
+  
+  Google trans=googletrans==3.1.0a0
+  
--- p1/main.py
+++ p1/main.py
@@ -1,28 +1,45 @@
 import time
 import math
 import ffmpeg
-from googletrans import Translator
 from faster_whisper import WhisperModel
+from pathlib import Path
+from openai import OpenAI
+from googletrans import Translator
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 translator = Translator()
 
+openaiAK = os.environ["OPENAI_API_KEY"]
+client = OpenAI(
+    organization=os.environ["OPENAI_ORG_ID"]
+)
+
 source_location = 'source/video/'
-#input_video = 'clase-15.mp4'
-input_video = '2.wav'
-#input_video = 'he.mp4'
+# input_video = 'clase-15.mp4'
+input_video = '68.mp4'
+# input_video = 'he.mp4'
 # input_video = 'audio-spa.wav'
-#input_video_name = input_video.replace(".mp4", "")
-input_video_name = input_video.replace(".wav", "")
+# input_video_name = input_video.replace(".mp4", "")
+input_video_name = input_video.replace(".mp4", "")
 extracted_audio_location = 'process/audio/'
 transcribed_text = 'transcribed.txt'
-out_lan = 'pol'
+out_lan = 'en'
 
 
 prompt = (
-    f"This is a podcast audio file which talks about data and teaches how to use Microsoft Power BI."
+    f"This is a podcast audio file that teaches how to use Microsoft Power BI."
+    f"It's language is hihly technical,  statistical language,  data language."
     f"Teacher is Sonia a female whose native language is spanish from Latin America.  Her talk is highly technical."
     f"This file will be transcribed and translated to multiple languages"
+    f"Teacher uses statistical language,  average, mode, mean, max, and many more statistical words and concepts."
+    f"Teacher uses a dataset Athletes Events contains data of Olympic events from 1896 to 2016."
+    f"It contains data about year of event, country, delegations, athletes ages, athletes height, weight, Body Mass Index BMI."
+    f"It contains the list of medals earned by each delegation."
 )
+
 
 def extract_audio():
     extracted_audio = f"audio-{input_video_name}.wav"
@@ -35,16 +52,17 @@ def extract_audio():
 
 def transcribe(audio):
     model = WhisperModel('medium')
-    segments, info = model.transcribe(audio, beam_size=5, initial_prompt=prompt)
+    segments, info = model.transcribe(
+        audio, beam_size=5, initial_prompt=prompt)
     language = info[0]
     print("Transcription Language ", info[0], info.language_probability)
     segments = list(segments)
-    # f = open('transcribed.txt', 'w')
+    f = open('transcribed.txt', 'w')
     for segment in segments:
         # print(segment)
         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end,
               translate_text(segment.text, 'es', 'en')))
-        # f.write(str(segment.text))
+        f.write(str(segment.text))
     return language, segments
 
 
@@ -52,6 +70,7 @@ def translate_text(text, input_lan, out_lan):
     return translator.translate(text, src=input_lan, dest=out_lan).text
     # f = open('res', 'w')
     # f.write(str(t))
+
 
 def format_time(seconds):
     hours = math.floor(seconds / 3600)
@@ -111,6 +130,18 @@ def add_subtitle_to_video(soft_subtitle, subtitle_file, subtitle_language):
         ffmpeg.run(stream, overwrite_output=True)
 
 
+def read_text(file):
+    f = open(file, 'r')
+    return str(f.read())
+
+def text_to_speech(text, language):
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="shimmer",
+        input=read_text("transcribed.txt")
+    )
+    #response.stream_to_file("translated-audio.mp3")
+
 def run():
     extracted_audio = extract_audio()
     language, segments = transcribe(extracted_audio)
@@ -122,5 +153,6 @@ def run():
         subtitle_file=subtitle_file,
         subtitle_language=language
     )
+    text_to_speech("transcribed.txt", 'en')
 
 run()
